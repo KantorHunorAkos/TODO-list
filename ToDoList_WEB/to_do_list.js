c6673bbf8cb3b93d6aa84442752553571a25bac4
HunorZalan <73434075+HunorZalan@users.noreply.github.com>
2020-11-16 12:03:09 +0200

    Add files via upload

---
+++ ToDoList_WEB/to_do_list.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAsn_dWrZNB1bRVul_XkgRWFgjEIusPKl8",
+    authDomain: "form-8954e.firebaseapp.com",
+    databaseURL: "https://form-8954e.firebaseio.com",
+    projectId: "form-8954e",
+    storageBucket: "form-8954e.appspot.com",
+    messagingSenderId: "347245119398",
+    appId: "1:347245119398:web:21766531a808a3d8ad7167",
+    measurementId: "G-C2LWPVJQFG"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
