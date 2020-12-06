5b2c96b8907f18e369c8cf1a98da5a20ae5147df
HunorZalan <73434075+HunorZalan@users.noreply.github.com>
2020-12-06 22:31:03 +0200

    Add files via upload

--- ToDoList_WEB/to_do_list.js
+++ ToDoList_WEB/to_do_list.js
@@ -7,7 +7,23 @@ var firebaseConfig = {
     messagingSenderId: "347245119398",
     appId: "1:347245119398:web:21766531a808a3d8ad7167",
     measurementId: "G-C2LWPVJQFG"
-  };
+};
+firebase.initializeApp(firebaseConfig);
 
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+function add_task(){
+    console.log("add_task");
+}
+
+function task_done(){
+    console.log("task_done");
+}
+
+function task_edit(){
+    console.log("task_edit");
+}
+
+function task_delete(){
+    console.log("task_delete");
+}
+
+
